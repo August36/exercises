e02d4fe57e2a2363f205d0c19d40f03e2aad3420
August <augustjp@live.dk>
2024-01-30 13:49:57 +0100

    cleaan

--- basic.js
+++ basic.js
@@ -1,10 +1,4 @@
 const randomNum = Math.floor(Math.random() * 100);
 
-console.log(randomNum);
-
 document.querySelector(".random_number").textContent = randomNum;
 
-
-// function generateRandomNumber() {
-
-// }

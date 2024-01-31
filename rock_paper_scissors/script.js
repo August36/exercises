33e4fea84bc83cd6ed83ade338205b14ba7ff1bc
August <augustjp@live.dk>
2024-01-31 19:53:31 +0100

    Eventlisteners added to each button

--- rock_paper_scissors/script.js
+++ rock_paper_scissors/script.js
@@ -2,6 +2,8 @@ const maxNum = 3;
 const minNum = 1;
 const randomNumber = Math.floor(Math.random() * (maxNum - minNum +1)) + minNum;
 
+console.log(randomNumber)
+
 const player1 = document.querySelector("#player1");
 const player2 = document.querySelector("#player2");
 
@@ -14,3 +16,20 @@ const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
 
+// Function to start the game - activates when a button is pressed
+function playGame() {
+
+}
+
+// Event listeners for buttons
+rock.addEventListener("click", function() {
+    playGame("rock");
+});
+
+paper.addEventListener("click", function() {
+    playGame("paper");
+});
+
+scissors.addEventListener("click", function() {
+    playGame("scissors");
+});

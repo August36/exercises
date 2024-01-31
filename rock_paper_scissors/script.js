ed7d5a5bc8cba5790d4f88631f30b537ecd9c7cd
August <augustjp@live.dk>
2024-01-31 20:16:27 +0100

    added 1 option for computer move

--- rock_paper_scissors/script.js
+++ rock_paper_scissors/script.js
@@ -1,8 +1,5 @@
 const maxNum = 3;
 const minNum = 1;
-const randomNumber = Math.floor(Math.random() * (maxNum - minNum +1)) + minNum;
-
-console.log(randomNumber)
 
 const player1 = document.querySelector("#player1");
 const player2 = document.querySelector("#player2");
@@ -16,20 +13,24 @@ const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
 
-// Function to start the game - activates when a button is pressed
-function playGame() {
-
-}
-
 // Event listeners for buttons
-rock.addEventListener("click", function() {
-    playGame("rock");
+rock.addEventListener("click", () => {
+    const randomNumber = Math.floor(Math.random() * (maxNum - minNum +1)) + minNum;
+    if (randomNumber === 1) {
+        console.log("rock");
+    }
 });
 
-paper.addEventListener("click", function() {
-    playGame("paper");
+paper.addEventListener("click", () => {
+    const randomNumber = Math.floor(Math.random() * (maxNum - minNum +1)) + minNum;
+    if (randomNumber === 2) {
+        console.log("paper")
+    };
 });
 
-scissors.addEventListener("click", function() {
-    playGame("scissors");
+scissors.addEventListener("click", () => {
+    const randomNumber = Math.floor(Math.random() * (maxNum - minNum +1)) + minNum;
+    if (randomNumber === 3) {
+        console.log("scissors")
+    };
 });

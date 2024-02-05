9075544e500798a810369421f9625de89d1d4182
August <augustjp@live.dk>
2024-02-05 17:43:54 +0100

    done

--- computer_guess_number/index.js
+++ computer_guess_number/index.js
@@ -1,7 +1,7 @@
 const minNum = 1;
 const maxNum = 100;
 
-const computerGuess = document.querySelector("#computerGuess");
+let computerGuess = document.querySelector("#computerGuess");
 
 const startGameBtn = document.querySelector("#startGameBtn");
 const correctAnswerBtn = document.querySelector("#correctAnswerBtn");
@@ -12,17 +12,53 @@ const resultMessage = document.querySelector("#result_message");
 
 let attempts = 0;
 
-// start game by adding eventlistener that activates runGame()
+// start game by adding an event listener that activates runGame()
 startGameBtn.addEventListener("click", function () {
-    runGame()
+    runGame();
 });
+
+function resetGame() {
+    attempts = 0;
+    resultMessage.textContent = "";
+}
 
 // Random number
 function generateRandomNumber() {
     return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 }
 
+tooLowBtn.addEventListener("click", function () {
+    attempts++;
+
+    // CurrentGuess is a string - this converts it to a number
+    const currentGuess = parseInt(computerGuess.textContent, 10);
+
+    // Generate a new guess between minNum and the previous computer guess
+    const newGuess = Math.floor(Math.random() * (maxNum - currentGuess + 1)) + currentGuess;
+
+    // Update computer guess
+    computerGuess.textContent = newGuess;
+});
+
+tooHighBtn.addEventListener("click", function () {
+    attempts++;
+
+    // CurrentGuess is a string - this converts it to a number
+    const currentGuess = parseInt(computerGuess.textContent, 10);
+
+    // Generate a new guess between minNum and the previous computer guess
+    const newGuess = Math.floor(Math.random() * (currentGuess - minNum + 1)) + minNum;
+
+    // Update computer guess
+    computerGuess.textContent = newGuess;
+});
+
+correctAnswerBtn.addEventListener("click", function () {
+    resultMessage.textContent = `Congratulations! It took ${attempts} attempts to guess the correct number.`;
+});
+
 function runGame() {
+    resetGame();
+    // generate first computerGuess
     computerGuess.textContent = generateRandomNumber();
 }
-

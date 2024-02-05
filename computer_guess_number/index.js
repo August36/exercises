aa4e5856994cfaa906559df83187fd0d6ab93c87
August <augustjp@live.dk>
2024-02-05 16:21:07 +0100

    javascript start

---
+++ computer_guess_number/index.js
@@ -0,0 +1,26 @@
+const minNum = 1;
+const maxNum = 100;
+
+const startGameBtn = document.querySelector("#startGameBtn");
+const correctAnswerBtn = document.querySelector("#correctAnswerBtn");
+const tooLowBtn = document.querySelector("#tooLowBtn");
+const tooHighBtn = document.querySelector("#tooHighBtn");
+
+const resultMessage = document.querySelector("#result_message");
+
+let attempts = 0;
+
+// start game by adding eventlistener that activates runGame()
+startGameBtn.addEventListener("click", function () {
+    runGame()
+});
+
+// Random number
+function generateRandomNumber() {
+    return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+}
+
+function runGame() {
+    
+}
+

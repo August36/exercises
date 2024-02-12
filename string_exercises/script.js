f252a783f85ba8668de33b605179c68be8d69ea5
August <augustjp@live.dk>
2024-02-12 09:36:05 +0100

    upper og lowercase

--- string_exercises/script.js
+++ string_exercises/script.js
@@ -1,20 +1,30 @@
-const aName = "Peter Heronimous Lundin";
+// const aName = "Peter Heronimous Lundin";
 
-const firstName = aName.substring(0, aName.indexOf(" "));
-console.log(firstName);
+// const firstName = aName.substring(0, aName.indexOf(" "));
+// console.log(firstName);
 
-const middleName = aName.substring(
-  aName.indexOf(" ") + 1,
-  aName.lastIndexOf(" ")
-);
-console.log(middleName);
+// const middleName = aName.substring(
+//   aName.indexOf(" ") + 1,
+//   aName.lastIndexOf(" ")
+// );
+// console.log(middleName);
 
-const lastName = aName.substring(aName.lastIndexOf(" ") + 1);
+// const lastName = aName.substring(aName.lastIndexOf(" ") + 1);
 
-console.log(lastName);
+// console.log(lastName);
 
-console.log(aName.split(" "));
+// console.log(aName.split(" "));
 
-const firstNameSplit = aName.split(" ")[0];
+// const firstNameSplit = aName.split(" ")[0];
 
 // split bruges til at lave en string om til et array
+
+// __________________________________________________________________________________________
+
+// Lav bogstaver i en string til upper/lowercase
+
+const aName = "LKadjaslkLKJ";
+
+console.log(
+  aName.slice(0, 2) + aName[2].toUpperCase() + aName.slice(3).toLowerCase()
+);
